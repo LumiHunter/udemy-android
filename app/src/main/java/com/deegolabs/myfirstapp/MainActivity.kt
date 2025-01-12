c25cf05978bfc9bbf9ee2e1d6e64fe22c21a4c61
Jieun Hwang <hwangje36@gmail.com>
2025-01-12 16:53:50 +0900

    Button, Box, Icon, DropdownMenu, Spacer, padding

--- app/src/main/java/com/deegolabs/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/deegolabs/myfirstapp/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.deegolabs.myfirstapp
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.widget.Space
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.deegolabs.myfirstapp.ui.theme.MyFirstAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,22 +55,48 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         // Here all the UI elements will be stacked below each other
-        Text("Unit Converter")
+        Text("Unit Converter", modifier = Modifier.padding(5.dp))
+        Spacer(modifier = Modifier.height(16.dp))   // density pixel, 화면의 픽셀 밀도에 의존하는 픽셀
         OutlinedTextField(value = "", onValueChange = {
             //Here goes what should happen, when the value of our OutlinedTextfield changed
         })
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
-            val context = LocalContext.current
-            // Here all the UI elements will be stacked next to each other
-            Button(onClick = { Toast
-                .makeText(context,
-                    "Thanks for clicking!",
-                    Toast.LENGTH_LONG).show() }) {
-                Text("Click me!")
+            Box{
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(text = { Text("Centimeters")}, onClick = {})
+                    DropdownMenuItem(text = { Text("Meters")}, onClick = {})
+                    DropdownMenuItem(text = { Text("Feet")}, onClick = {})
+                    DropdownMenuItem(text = { Text("Millimeters")}, onClick = {})
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = true, onDismissRequest = {}) {
+                    DropdownMenuItem(text = { Text("Centimeters")}, onClick = {})
+                    DropdownMenuItem(text = { Text("Meters")}, onClick = {})
+                    DropdownMenuItem(text = { Text("Feet")}, onClick = {})
+                    DropdownMenuItem(text = { Text("Millimeters")}, onClick = {})
+                }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result:")
     }
 }
