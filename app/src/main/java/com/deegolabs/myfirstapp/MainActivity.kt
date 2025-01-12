89f68c18221fd5182523e55e28bfe56ab113f55e
Jieun Hwang <hwangje36@gmail.com>
2025-01-12 14:43:45 +0900

    context, Button onClick Toast

--- app/src/main/java/com/deegolabs/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/deegolabs/myfirstapp/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.deegolabs.myfirstapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.deegolabs.myfirstapp.ui.theme.MyFirstAppTheme
 
@@ -19,11 +27,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyFirstAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "JiEun",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    UnitConverter()
                 }
             }
         }
@@ -31,17 +40,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Welcome $name!",
-        modifier = modifier
-    )
+fun UnitConverter() {
+    Column {
+        // Here all the UI elements will be stacked below each other
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {
+            //Here goes what should happen, when the value of our OutlinedTextfield changed
+        })
+        Row {
+            val context = LocalContext.current
+            // Here all the UI elements will be stacked next to each other
+            Button(onClick = { Toast
+                .makeText(context,
+                    "Thanks for clicking!",
+                    Toast.LENGTH_LONG).show() }) {
+                Text("Click me!")
+            }
+        }
+        Text("Result:")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyFirstAppTheme {
-        Greeting("Denis")
-    }
+fun UnitConverterPreview() {
+    UnitConverter()
 }
